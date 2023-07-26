b609b19db6377b5a6cea8a6bfbc823bcae562184
jessicaayers <jpayers2@ncsu.edu>
2023-07-26 17:13:22 -0400

    Updated about and explore page

--- ZooAnimalLifespan/server.R
+++ ZooAnimalLifespan/server.R
@@ -1,14 +1,12 @@
 library(shiny)
+library(tidyverse)
+library(DT)
+
+animal <- read.csv("/Users/jessayers/Documents/ST 558/TOPIC 4/AZA_MLE_Jul2018.csv")
+animal <- animal %>% select(-c(Male.Data.Deficient, Female.Data.Deficient))
 
 function(input, output, session) {
   #About Page
-  output$purposeOutput <- renderText({
-    "The purpose of this app to explore data relative to the median lifespans of different zoo animals. This app provides a platform to explore and visualize the data, fit three different supervised learning models to the data, and investigate and subset the actual data itself."
-  })
-  
-  output$sourceOutput <- renderText({
-    "The data was found on the data world platform. The data was downloaded as a csv file. The variables included are: species common name, species scientific name, taxon class, overall sample size, overall median life expectancy, overall lower confidence interval endpoint, overall upper confidence interval endpoint, male sample size, male median life expectancy, male lower confidence interval endpoint, male upper confidence interval endpoint, female sample size, female median life expectancy, female lower confidence interval endpoint, female upper confidence interval endpoint, male data deficient, and female data deficient."
-  })
   
   output$link <- renderUI({
     tagList("More information on the data can be found here: ", a("Zoo Animal Lifespans", href = "https://data.world/animals/zoo-animal-lifespans"))
@@ -18,18 +16,52 @@ function(input, output, session) {
     tagList("The article this data was used for can be found here: ", a("Sex-specific median life expectancies from ex situ populations for 330 animal species", href = "https://www.nature.com/articles/sdata201919#Sec1"))
   })
   
-  output$tabOutput <- renderText({
-    "The purpose of this page is to give the user a sense of what data will be used and the overall goal of the app. The second page will provide the user with an interactive platform for data exploration. The third page allows the user to fit three different supervised learning methods. Within this page, there are separate tabs for information on the models, model fits, and predicted values. The last and final page gives the user direct access to the data set. The user is able to subset and save the data here."
-  })
-  
   output$pictureOutput <- renderImage({
     list(src = "/Users/jessayers/Documents/ST 558/TOPIC 4/zooanimal.jpeg",
-         width = "1000", height = "400", deleteFile = FALSE)
+         width = "1000", height = "400")
   })
   
-  output$expOutput <- renderText({
-    "Exploration Page"
+  #Explore Page
+  output$sumPlot <- renderPlot({
+    if(input$type == "Taxon Class"){
+      barplot(table(animal$TaxonClass), col = "blue")
+    }
+    else if(input$type == "Median Life Expectancy"){
+      if(input$median == "Overall"){
+        hist(animal$Overall.MLE)
+      }
+      else if(input$median == "Female"){
+        hist(animal$Female.MLE)
+      }
+      else if(input$median == "Male"){
+        hist(animal$Male.MLE)
+      }
+    }
   })
+  
+  output$sumTable <- renderDataTable({
+    if(input$type == "Taxon Class"){
+      tab <- as.data.frame(table(animal$TaxonClass))
+      rename(tab, "Taxon Class" = Var1, "Count" = Freq)
+    }
+    else if(input$type == "Median Life Expectancy"){
+      if(input$median == "Overall"){
+        print(animal %>% select(animal$Species.Common.Name, animal$Overall.MLE))
+      }
+      else if(input$median == "Female"){
+        animal %>% select(animal$Species.Common.Name, animal$Female.MLE)
+      }
+      else if(input$median == "Male"){
+        animal %>% select(animal$Species.Common.Name, animal$Male.MLE)
+      }
+    }
+  })
+  
+  
+  
+  #hist(animal$Overall.MLE)
+  #hist(animal$Male.MLE)
+  #hist(animal$Female.MLE)
   
   #Modeling Tab
   output$infoOutput <- renderText({
--- ZooAnimalLifespan/ui.R
+++ ZooAnimalLifespan/ui.R
@@ -1,28 +1,45 @@
 library(shiny)
+library(DT)
 
 fluidPage(
   
   titlePanel("Zoo Animal Lifespans"),
   
   tabsetPanel(
+    #About Page
     tabPanel("About", 
              fluid = TRUE,
-             mainPanel(textOutput("purposeOutput"),
-                       br(),
-                       textOutput("sourceOutput"),
-                       br(),
-                       uiOutput("link"),
-                       br(),
-                       uiOutput("link2"),
-                       br(),
-                       textOutput("tabOutput"),
-                       br(),
-                       imageOutput("pictureOutput"))
+             mainPanel( "The purpose of this app to explore data relative to the median lifespans of different zoo animals. This app provides a platform to explore and visualize the data, fit three different supervised learning models to the data, and investigate and subset the actual data itself.",
+                        br(),
+                        br(),
+                        "The data was found on the data world platform. The data was downloaded as a csv file. The variables included are: species common name, species scientific name, taxon class, overall sample size, overall median life expectancy, overall lower confidence interval endpoint, overall upper confidence interval endpoint, male sample size, male median life expectancy, male lower confidence interval endpoint, male upper confidence interval endpoint, female sample size, female median life expectancy, female lower confidence interval endpoint, female upper confidence interval endpoint, male data deficient, and female data deficient.",
+                        br(),
+                        br(),
+                        uiOutput("link"),
+                        br(),
+                        uiOutput("link2"),
+                        br(),
+                        "The purpose of this page is to give the user a sense of what data will be used and the overall goal of the app. The second page will provide the user with an interactive platform for data exploration. The third page allows the user to fit three different supervised learning methods. Within this page, there are separate tabs for information on the models, model fits, and predicted values. The last and final page gives the user direct access to the data set. The user is able to subset and save the data here.",
+                        br(),
+                        br(),
+                        imageOutput("pictureOutput"))
     ),
-    
+    #Explore Page
     tabPanel("Exploration",
              fluid = TRUE,
-             mainPanel(textOutput("expOutput"))),
+             sidebarPanel(
+               "Let's explore some data!",
+               br(),
+               br(),
+               selectInput("type", label = "Variable Selection", c("Taxon Class", "Median Life Expectancy")),
+               conditionalPanel(condition = "input.type == 'Median Life Expectancy'",
+                                selectInput("median", label = "Specified MLE", c("Overall", "Female", "Male")))
+             ),
+             mainPanel(plotOutput("sumPlot"),
+                       dataTableOutput("sumTable"))
+             
+    )
+    ,
     
     tabPanel("Modeling",
              fluid = TRUE,
